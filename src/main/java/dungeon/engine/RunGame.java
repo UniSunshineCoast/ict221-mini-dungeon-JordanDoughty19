23ec08ea83c13a8fa7f68a4b1c8c97fee442f965
JordanDoughty19 <150669470+JordanDoughty19@users.noreply.github.com>
2025-05-29 19:53:49 +1000

    Create RunGame.java

---
+++ src/main/java/dungeon/engine/RunGame.java
@@ -0,0 +1,8 @@
+import dungeon.engine.GameEngine;
+
+public class RunGame {
+    public static void main(String[] args) {
+        GameEngine engine = new GameEngine();
+        engine.play();
+    }
+}
