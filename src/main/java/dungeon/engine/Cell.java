3b452e78fd98707e2ff727e9ea18e1b28484d941
JordanDoughty19 <150669470+JordanDoughty19@users.noreply.github.com>
2025-05-29 19:52:54 +1000

    Update Cell.java

--- src/main/java/dungeon/engine/Cell.java
+++ src/main/java/dungeon/engine/Cell.java
@@ -5,3 +5,23 @@ import javafx.scene.layout.StackPane;
 public class Cell extends StackPane {
 
 }
+
+public class Cell {
+    private Item item;
+
+    public Cell(Item item) {
+        this.item = item;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void clearItem() {
+        this.item = null;
+    }
+
+    public char getSymbol() {
+        return (item != null) ? item.getSymbol() : '.';
+    }
+}
